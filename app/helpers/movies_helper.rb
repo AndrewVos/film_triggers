3dffa9ab9368b0ee3463d5e8a72bc920fd4d0018
Andrew Vos <andrew.vos@gmail.com>
2017-05-07 16:29:10 +0100

    Add bootstrap and style up movies#show and movies#index

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+  def poster_url_for(poster_path)
+    return 'http://placehold.it/92x138' unless poster_path.present?
+    "https://image.tmdb.org/t/p/w92#{poster_path}"
+  end
+
+  def large_poster_url_for(poster_path)
+    return 'http://placehold.it/300x450' unless poster_path.present?
+    "https://image.tmdb.org/t/p/w300#{poster_path}"
+  end
 end
